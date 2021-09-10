b15e866f859ba746186cec7498b13297ac34a756
Robert Jones <22344413+Robert-G-J@users.noreply.github.com>
2021-09-10 15:28:39 +0100

    Refactor to push unsafeRun out

--- exercises/src/main/scala/exercises/action/fp/IO.scala
+++ exercises/src/main/scala/exercises/action/fp/IO.scala
@@ -20,12 +20,8 @@ trait IO[A] {
   // action3.unsafeRun()
   // prints "Fetching user", fetches user 1234 from db and returns it.
   // Note: There is a test for `andThen` in `exercises.action.fp.IOTest`.
-  def andThen[Other](other: IO[Other]): IO[Other] = {
-    IO {
-      this.unsafeRun()
-      other.unsafeRun()
-    }
-  }
+  def andThen[Other](other: IO[Other]): IO[Other] =
+      this.flatMap(_ => other)
 
   // Popular alias for `andThen` (cat-effect, Monix, ZIO).
   // For example,
