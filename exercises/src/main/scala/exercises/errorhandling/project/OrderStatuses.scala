d72b64e548abf25587ca5f8ee7442ccd70307b37
Robert Jones <22344413+Robert-G-J@users.noreply.github.com>
2022-07-29 14:47:39 +0100

    Removes outdated status type

--- exercises/src/main/scala/exercises/errorhandling/project/OrderStatuses.scala
+++
@@ -1,8 +0,0 @@
-package exercises.errorhandling.project
-
-object OrderStatuses {
-  val Draft: String = "Draft"
-  val Checkout: String = "Checkout"
-  val Submitted: String = "Submitted"
-  val Delivered: String = "Delivered"
-}
