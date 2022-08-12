5b9bd1c55454c8ca1d0f20bd86a5e990ce5705c7
Robert Jones <22344413+Robert-G-J@users.noreply.github.com>
2022-08-12 14:17:02 +0100

    Use smart constructor for Quantity, which can only be a pos int

--- exercises/src/main/scala/exercises/errorhandling/project/Item.scala
+++ exercises/src/main/scala/exercises/errorhandling/project/Item.scala
@@ -1,3 +1,11 @@
 package exercises.errorhandling.project
 
-case class Item(id: String, quantity: Int, unitPrice: Double)
+case class Item(id: String, quantity: Quantity, unitPrice: BigDecimal)
+
+case class Quantity(value: Nat)
+
+final case class Nat(n: Int)
+object Nat {
+  def fromInt(n: Int): Option[Nat] =
+    if (n >= 0) Some(new Nat(n)) else None
+}
